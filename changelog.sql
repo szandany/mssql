5653e79ec1d466d64765324e28c725851ff54866
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2022-03-04 10:49:31 -0600

    Update changelog.sql

--- changelog.sql
+++ changelog.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql changeLogId:e2baab75-eb98-40c0-848b-e4c758fd39bf
 
---changeset SteveZ:createTable_salesTableZ context:"DEV" labels:"staging,hotfix" runwith:sqlcmd
+--changeset SteveZ:createTable_salesTableZ context:"DEV" labels:"staging,hotfix"
 CREATE TABLE salesTableZ (
    ID int NOT NULL,
    NAME varchar(20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
